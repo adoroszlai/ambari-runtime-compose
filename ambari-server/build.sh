97b7b4a0df60fd72a702bbf602422466f11cbb26
Doroszlai, Attila <adoroszlai@apache.org>
2018-02-17 16:13:18 +0100

    Move CONF_DIR and working directory definition to Dockerfile

--- ambari-server/build.sh
+++ ambari-server/build.sh
@@ -9,5 +9,3 @@ maven_url=$(curl -LSs http://www.apache.org/dyn/closer.cgi/maven/maven-3/${MAVEN
 : ${maven_url:="http://www.apache.org/dist/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz"}
 echo "Downloading Maven from ${maven_url}"
 curl -LSs "${maven_url}" | tar -xzf - --strip-components 1 -C $MAVEN_HOME
-
-mkdir -p /etc/ambari-server/conf
