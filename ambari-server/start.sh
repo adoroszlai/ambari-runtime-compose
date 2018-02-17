6303d8eca4e8f2727fcad61d55a931b9ae1a675d
Doroszlai, Attila <adoroszlai@apache.org>
2018-02-17 11:33:48 +0100

    Reduce the number of custom directories

--- ambari-server/start.sh
+++ ambari-server/start.sh
@@ -8,7 +8,7 @@ ambariFullVersion=${ambariVersion}-${buildNumber}
 export ambariFullVersion ambariVersion buildNumber
 
 version_file=$(fgrep server.version.file /etc/ambari-server/conf/ambari.properties | cut -f2 -d=)
-: ${version_file:=/var/lib/ambari-server/resources/version}
+: ${version_file:=/var/lib/ambari-server/version}
 echo "$ambariVersion" > $version_file
 
 cp_file=/tmp/ambari-server.classpath
@@ -16,6 +16,13 @@ cp_file=/tmp/ambari-server.classpath
 if [[ ! -f $cp_file ]] || [[ pom.xml -nt $cp_file ]]; then
   mvn -Dmaven.repo.local=/opt/maven_repo -Dmdep.outputFile=$cp_file dependency:build-classpath \
     && touch $cp_file # make sure it's newer even if "No changes found."
+fi
+
+views_dir=$(fgrep views.dir /etc/ambari-server/conf/ambari.properties | cut -f2 -d=)
+: ${views_dir:=/var/lib/ambari-server/views}
+if [[ ! -d ${views_dir} ]]; then
+  mkdir -p ${views_dir}
+  cp /opt/ambari/ambari-admin/target/ambari-admin*jar ${views_dir}/
 fi
 
 java \
