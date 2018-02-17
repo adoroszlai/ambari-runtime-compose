ee8497c491b7ad0d3a27f7fc052c8ffa49fc73b6
Doroszlai, Attila <adoroszlai@apache.org>
2018-02-17 11:30:20 +0100

    Parametrize ambari-agent version number

--- ambari-agent/centos/6/start.sh
+++ ambari-agent/centos/6/start.sh
@@ -2,11 +2,14 @@
 
 set -e -u
 
+: ${ambariVersion:=2.0.0.0}
+: ${buildNumber:=SNAPSHOT}
+
+export ambariVersion buildNumber
+
 mkdir -p /var/lib/ambari-agent/{cache,cred,data,keys} /var/log/ambari-agent /var/run/ambari-agent
 export PATH=$PATH:/usr/lib/ambari-agent/bin
 #python /opt/ambari/ambari-agent/src/main/python/ambari_agent/main.py
-export ambariVersion=3.0.0
-export buildNumber=123
 ambari-agent start
 while true; do
   sleep 3
