8a264d028692834d30d8e5aae0e6e3176a77672d
Doroszlai, Attila <adoroszlai@apache.org>
2018-02-17 11:33:05 +0100

    Create ambari-server version file

--- ambari-server/start.sh
+++ ambari-server/start.sh
@@ -2,6 +2,15 @@
 
 set -e -u
 
+: ${ambariVersion:=2.0.0.0}
+: ${buildNumber:=SNAPSHOT}
+ambariFullVersion=${ambariVersion}-${buildNumber}
+export ambariFullVersion ambariVersion buildNumber
+
+version_file=$(fgrep server.version.file /etc/ambari-server/conf/ambari.properties | cut -f2 -d=)
+: ${version_file:=/var/lib/ambari-server/resources/version}
+echo "$ambariVersion" > $version_file
+
 cp_file=/tmp/ambari-server.classpath
 
 if [[ ! -f $cp_file ]] || [[ pom.xml -nt $cp_file ]]; then
