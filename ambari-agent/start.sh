359147bccf68722d51123ed286a0882e9f044811
Doroszlai, Attila <adoroszlai@apache.org>
2018-02-18 16:17:59 +0100

    Avoid having to define AMBARI_SERVER_RESOURCES variable

--- ambari-agent/start.sh
+++ ambari-agent/start.sh
@@ -9,7 +9,9 @@ export ambariVersion buildNumber
 
 mkdir -p /var/lib/ambari-agent/{cache,cred,data,keys,tmp} /var/log/ambari-agent /var/run/ambari-agent
 echo ${ambariVersion} > /var/lib/ambari-agent/data/version
-[[ -n ${AMBARI_SERVER_RESOURCES} ]] && [[ -e ${AMBARI_SERVER_RESOURCES} ]] && cp -r ${AMBARI_SERVER_RESOURCES}/{common-services,stacks,mpacks} /var/lib/ambari-agent/cache/
+
+AMBARI_SERVER_RESOURCES=/opt/ambari-server/resources
+[[ -e ${AMBARI_SERVER_RESOURCES} ]] && cp -r ${AMBARI_SERVER_RESOURCES}/{common-services,stacks,mpacks} /var/lib/ambari-agent/cache/
 
 export PATH=$PATH:/usr/lib/ambari-agent/bin
 
